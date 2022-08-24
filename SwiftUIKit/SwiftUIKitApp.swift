646ced82632143f2c1230ef57712059d540c3cd3
Vitor Moriya <vitorjmoriya@gmail.com>
2022-08-24 09:18:51 -0300

    feat(git): initial commit

---
+++ SwiftUIKit/SwiftUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKitApp.swift
+//  SwiftUIKit
+//
+//  Created by Vitor Moriya on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
