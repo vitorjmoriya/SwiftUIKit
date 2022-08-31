98da05c8d060a13a72516fa8b49c17e4d4b003ae
Vitor Moriya <vitorjmoriya@gmail.com>
2022-08-31 10:04:00 -0300

    feat(Introspect): add library & example

--- SwiftUIKit/ContentView.swift
+++ SwiftUIKit/ContentView.swift
@@ -5,21 +5,33 @@
 //  Created by Vitor Moriya on 24/08/22.
 //
 
+import Introspect
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
+    @ObservedObject var viewModel: ViewModel
 
     var body: some View {
         Form {
-            TextField("Enter your name", text: $name)
-            Text("Your name is \(name)")
+            TextField("Enter your name", text: $viewModel.name)
+            Text("Your name is \(viewModel.name)")
         }
+        .introspectTableView { tableView in
+            tableView.delegate = viewModel.tableDelegate
+        }
+    }
+}
+
+extension ContentView {
+    class ViewModel: ObservableObject {
+        var tableDelegate: UITableViewDelegate?
+
+        @Published var name: String = ""
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: .init())
     }
 }
--- SwiftUIKit/MainController.swift
+++ SwiftUIKit/MainController.swift
@@ -6,15 +6,25 @@
 //
 
 import SwiftUI
-class MainController: UIHostingController<ContentView> {
+class MainController: UIHostingController<ContentView>, UITableViewDelegate {
+
+    let viewModel: ContentView.ViewModel
 
     init() {
-        let view = ContentView()
+        let viewModel: ContentView.ViewModel = .init()
 
-        super.init(rootView: view)
+        self.viewModel = viewModel
+
+        super.init(rootView: .init(viewModel: viewModel))
+
+        viewModel.tableDelegate = self
     }
 
     @MainActor required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Table cell number \(indexPath.row) was tapped!")
     }
 }
